3d61dbdb187950bfd45b238ea1967f92eda78b8a
Nipun Jyoti Pathak <73539736+nipunjyoti@users.noreply.github.com>
2022-06-20 02:20:54 +0530

    Update SpaceTimeGLOBE.m

--- Space-Time Globe/SpaceTimeGLOBE.m
+++ Space-Time Globe/SpaceTimeGLOBE.m
@@ -8,7 +8,7 @@ grid on;
 
 % Inputs
 fprintf("\nVelocity wrt rest observer is v0 and vm...\nFrom the frame of reference of v0 coordinate is shifted...\n\n");
-V = input("[v0, vm] = ");
+V = input("[Va, Vb] = ");
 v0 = V(1); vm = V(2);
 
 % Conditions for relative velocity
@@ -23,6 +23,11 @@ gamma = 1/sqrt(1-vr^2);
 % Change of coordinate
 tnew = gamma*(t - vr*x);
 xnew = gamma*(x - vr*t);
+% Moving frame coordinate wrt rest observer
+tmov = gamma*(t + v0*x);
+xmov = gamma*(x + v0*t);
+tmov1 = gamma*(t + vr*x);
+xmov1 = gamma*(x + vr*t);
 t0 = t/gamma;
 
 % Label the globe
@@ -32,18 +37,21 @@ ylabel('Y(Time)',FontSize=30);
 
 % Plot the lines
 hold on;
-plot(0*t,t,'g',-v0*tnew,tnew,'g',  -c*t,t,'*',c*t,t,'*', LineWidth=1);
+plot(0*t,t,'g',-v0*tnew,tnew,'g', LineWidth=2);
+plot(-c*t,t,Color='#ffcc00', LineWidth=2);
+plot(c*t,t,Color='#ffcc00', LineWidth=2);
 plot(v0*t,t,'r',0*t0,t0,'r', LineStyle='--',LineWidth=3);
-plot(,vm*t,t,'b',vr*tnew,tnew,'b', LineStyle='--',LineWidth=2);
-plot(xnew,tnew,'y');
+plot(vm*t,t,'b',vr*tnew,tnew,'b', LineStyle='--',LineWidth=2);
+plot(xmov,tmov,Color='#ffcc33');
+plot(xmov1,tmov1,Color='#cc99ff');
 
 % Label the lines
 text(c*60,-60,"Light",FontSize=15,FontWeight="bold");
 text(-c*60,-60,"Light",FontSize=15,FontWeight="bold");
-text(v0*50,50,"O",FontSize=15,FontWeight="bold");
-text(0,60,"O'",FontSize=15,FontWeight="bold");
-text(vm*40,40,"M",FontSize=15,FontWeight="bold");
-text(vr*75,75,"M'",FontSize=15,FontWeight="bold");
+text(v0*50,50,"A",FontSize=15,FontWeight="bold");
+text(0,60,"A'",FontSize=15,FontWeight="bold");
+text(vm*40,40,"B",FontSize=15,FontWeight="bold");
+text(vr*75,75,"B'",FontSize=15,FontWeight="bold");
 text(-v0*100,100,"Rest'",FontSize=15,FontWeight="bold");
 text(0,78,"Rest",FontSize=15,FontWeight="bold");
 
