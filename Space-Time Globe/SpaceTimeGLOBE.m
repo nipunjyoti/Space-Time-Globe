24c5204606d56681e4aec48434317e92f26d19e1
Nipun Jyoti Pathak <73539736+nipunjyoti@users.noreply.github.com>
2022-06-20 22:25:54 +0530

    Update SpaceTimeGLOBE.m

--- Space-Time Globe/SpaceTimeGLOBE.m
+++ Space-Time Globe/SpaceTimeGLOBE.m
@@ -7,7 +7,7 @@ yline(Y,LineWidth=2);
 xline(X,LineWidth=2);
 
 % Create the grids
-[x,t] = meshgrid(0:5, 0:5);
+[x,t] = meshgrid(0:1, 0:1);
 grid on;
 
 % Inputs
@@ -29,16 +29,16 @@ gamma = 1/sqrt(1-vr^2);
 tnew = gamma*(t - vr*x);
 xnew = gamma*(x - vr*t);
 % Moving frame coordinate wrt rest observer
-tmovA = gamma*(t + v0*x);
-xmovA = gamma*(x + v0*t);
+tmovA = (t + v0*x)/sqrt(1-v0^2);
+xmovA = (x + v0*t)/sqrt(1-v0^2);
 
-tmovB = gamma*(t + vm*x);
-xmovB = gamma*(x + vm*t);
+tmovB = (t + vm*x)/sqrt(1-vm^2);
+xmovB = (x + vm*t)/sqrt(1-vm^2);
 
 tmovR = gamma*(t + vr*x);
 xmovR = gamma*(x + vr*t);
-t0 = t/gamma;
-x0 = x*gamma;
+t0 = t*sqrt(1-v0^2);
+x0 = x/sqrt(1-v0^2);
 
 % Label the globe
 title('Space-Time Globe',FontSize=30,FontWeight='bold')
@@ -50,7 +50,8 @@ hold on;
 plot(-c*t,t,Color='#002050', LineWidth=3);
 plot(c*t,t,Color='#002050', LineWidth=3);
 plot(v0*t,t,'r',LineWidth=3);
-plot(0*t0,t0, Color='#e87200', LineWidth=3)
+plot(0*t0,t0, Color='#e87200', LineWidth=3);
+plot(x0,0*t0, Color='#e87200', LineWidth=3);
 plot(vm*t,t,'b',LineWidth=3);
 plot(vr*tnew,tnew,Color='#54b5fb',LineWidth=3);
 
@@ -67,11 +68,11 @@ plot(x,t,Color='#107610');
 plot(t,x,Color='#107610');
 
 % Label the lines
-text(c*3.5,3.5,"Light",FontSize=15,FontWeight="bold",Color='#002050');
-text(-c*3.5,3.5,"Light",FontSize=15,FontWeight="bold",Color='#002050');
-text(v0*4,4,"A",FontSize=15,FontWeight="bold",Color='r');
-text(0,3,"A'",FontSize=15,FontWeight="bold",Color='#e87200');
-text(vm*3,3,"B",FontSize=15,FontWeight="bold",Color='b');
-text(vr*7,7,"B'",FontSize=15,FontWeight="bold",Color='#3049ad');
+text(c*0.7,0.7,"Light",FontSize=15,FontWeight="bold",Color='#002050');
+text(-c*0.7,0.7,"Light",FontSize=15,FontWeight="bold",Color='#002050');
+text(v0*1,1,"A",FontSize=15,FontWeight="bold",Color='r');
+text(0,0.6,"A'",FontSize=15,FontWeight="bold",Color='#e87200');
+text(vm*0.7,0.7,"B",FontSize=15,FontWeight="bold",Color='b');
+text(vr*1.3,1.3,"B'",FontSize=15,FontWeight="bold",Color='#3049ad');
 
 
