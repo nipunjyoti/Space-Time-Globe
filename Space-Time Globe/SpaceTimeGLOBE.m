932afabdb7646aaf26d22e52c03cc942f8e27364
Nipun Jyoti Pathak <73539736+nipunjyoti@users.noreply.github.com>
2022-06-21 15:55:35 +0530

    Update SpaceTimeGLOBE.m

--- Space-Time Globe/SpaceTimeGLOBE.m
+++ Space-Time Globe/SpaceTimeGLOBE.m
@@ -12,6 +12,11 @@ g=G(1);
 [x,t] = meshgrid(0:g, 0:g);
 grid on;
 
+% Label the globe
+title('Space-Time Globe',FontSize=30,FontWeight='bold')
+xlabel('X(Space)',FontSize=30);
+ylabel('Y(Time)',FontSize=30);
+
 % Inputs
 fprintf("\n\nVelocity wrt rest observer(Green Sheet) is Va(Red Sheet) and Vb(Blue Sheet)...\nFrom the frame of reference of Va coordinate is shifted...\nSpeed of light is taken as 1...\n\n");
 V = input("[Va, Vb] = ");
@@ -43,11 +48,6 @@ xmovBR = gamma*(x + vr*t);
 tmovAR = gamma*(t - vr*x);
 xmovAR = gamma*(x - vr*t);
 
-% Label the globe
-title('Space-Time Globe',FontSize=30,FontWeight='bold')
-xlabel('X(Space)',FontSize=30);
-ylabel('Y(Time)',FontSize=30);
-
 % Plot the lines
 hold on;
 plot(-c*t,t,Color='#ecc864', LineWidth=4);%Light
@@ -58,8 +58,16 @@ plot(vr*tmovAR,tmovAR,Color='#54b5fb',LineWidth=4);%Line B'
 plot(-vr*tmovBR,tmovBR,Color='#9b0065',LineWidth=4);%Line A'
 plot(x*0,t,Color='#107610',LineWidth=4);%Rest perpective line
 
+% Label the lines
+text(c*g,g,"Light",FontSize=15,FontWeight="bold",Color='#ccaa44');%Light
+text(-c*g,g,"Light",FontSize=15,FontWeight="bold",Color='#ccaa44');%Light
+text(v0*(g-0.7),(g-0.7),"A",FontSize=15,FontWeight="bold",Color='r');%A
+text(-vr*(g+0.3),(g+0.3),"A'",FontSize=15,FontWeight="bold",Color='#b73a3a');%A'
+text(vm*g,g,"B",FontSize=15,FontWeight="bold",Color='b');%B
+text(vr*g,g,"B'",FontSize=15,FontWeight="bold",Color='#54b5fb');%B'
+text(0*(g-0.5),(g-0.5),"RP",FontSize=15,FontWeight="bold",Color='#107610');%Rest Perspective
 
-%Space-Time Grids
+% Space-Time Grids
 plot(xmovA,tmovA,Color='r',LineWidth=1,LineStyle='--');%Space-Time Sheet of A wrt Rest observer
 plot(tmovA,xmovA,Color='r',LineWidth=1,LineStyle='--');%Space-Time Sheet of A wrt Rest observer
 
@@ -74,13 +82,4 @@ plot(tmovB,xmovB,Color='b',LineWidth=1,LineStyle='--');%Space-Time Sheet of B wr
 
 plot(x,t,Color='#107610',LineWidth=1,LineStyle='--');%Space-Time Sheet of A or B wrt them
 plot(t,x,Color='#107610',LineWidth=1,LineStyle='--');%Space-Time Sheet of A or B wrt them
-
-% Label the lines
-text(c*g,g,"Light",FontSize=15,FontWeight="bold",Color='#ccaa44');%Light
-text(-c*g,g,"Light",FontSize=15,FontWeight="bold",Color='#ccaa44');%Light
-text(v0*(g-0.7),(g-0.7),"A",FontSize=15,FontWeight="bold",Color='r');%A
-text(-vr*(g+0.3),(g+0.3),"A'",FontSize=15,FontWeight="bold",Color='#b73a3a');%A'
-text(vm*g,g,"B",FontSize=15,FontWeight="bold",Color='b');%B
-text(vr*g,g,"B'",FontSize=15,FontWeight="bold",Color='#54b5fb');%B'
-text(0*(g-0.5),(g-0.5),"RP",FontSize=15,FontWeight="bold",Color='#107610');%Rest Perspective
 
